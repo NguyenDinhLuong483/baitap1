6d4f34c9faa7dc0bf0db65b9af7443f9312483ab
NguyenDinh Luong <dinhluong040803@gmail.com>
2023-07-26 16:34:29 +0700

    first commit

---
+++ Baitap1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Baitap1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(10); // in ra một số nguyên
+            Console.WriteLine(10.0); // in ra một số thực
+            Console.WriteLine(true); // in ra một giá trị logic
+            Console.WriteLine("Hello world"); // in ra một chuỗi
+            int a = 100;
+            Console.WriteLine(a); // in ra một biến kiểu số nguyên
+            bool b = false;
+            Console.WriteLine(b); // in ra một biến kiểu logic
+            string c = "Hi there!";
+            Console.WriteLine(c); // in ra một biến kiểu xâu ký tự
+            string thongbao = "Xin chao cac ban";
+            Console.WriteLine(thongbao);
+        }
+    }
+}
